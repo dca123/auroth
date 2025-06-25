04d847df13c749f29900e4e810dff4e6b7bfa443
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-26 00:17:13 +0530

    vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [tsConfigPaths(), tanstackStart()],
+  plugins: [tanstackStart({ target: 'vercel' })],
 })
