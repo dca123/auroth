0c1de8880907213fef71450fb2190986d46f212e
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-26 13:03:55 +0530

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     // tanstackStart(),
-    tanstackStart({ target: "vercel" }),
+    tanstackStart({ target: "netlify" }),
   ],
 });
